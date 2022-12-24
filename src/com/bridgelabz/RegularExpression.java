25f344bd11971522f8baefc1be440dac87741081
Supriya Bhurle <supriyabhurle28@gmail.com>
2022-12-24 14:51:57 +0530

    Test password Rule 4

--- src/com/bridgelabz/RegularExpression.java
+++ src/com/bridgelabz/RegularExpression.java
@@ -4,10 +4,10 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegularExpression {
-    static Pattern pattern = Pattern.compile("(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}");
+    static Pattern pattern = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=[^@#$%^&*+=]*[@#$%^&*+=][^@#$%^&*+=]*$).{8,}$");
     public static void main(String[] args)
     {
-        Matcher matcher = pattern.matcher("Supriyabhurle28");
+        Matcher matcher = pattern.matcher("Supriyabhurle28@");
         if(matcher.matches())
             System.out.println(true);
         else
